5ae76be69e8ccc51ed76472acaf54e0370632e83
RanimyLeite <ranimy.leitecosta@gmail.com>
2022-12-19 16:02:08 -0300

    refact - criando validações e exceção

--- ConsoleWebScraping/IbovespaWebScraping.cs
+++ ConsoleWebScraping/IbovespaWebScraping.cs
@@ -13,23 +13,40 @@ namespace ConsoleWebScraping
     {
         public IbovespaInfoDTO GetIbovespaInfo()
         {
-            HtmlWeb web = new HtmlWeb();
+            try
+            {
+                HtmlWeb web = new HtmlWeb();
 
-            //Carrega o site onde se quer pegar as informações
-            HtmlDocument doc = web.Load("https://www.infomoney.com.br/cotacoes/b3/indice/ibovespa/"); 
+                //Carrega o site onde se quer pegar as informações
+                HtmlDocument doc = web.Load("https://www.infomoney.com.br/cotacoes/b3/indice/ibovespa/");
 
-            //Passa como path o que queremos pegar no site
-            //No caso ele vai selecionar a div com class=value e o p dentro dela
-            var ibovespaPointsNode = doc.DocumentNode.SelectNodes("//div[@class='value']/p");
-            var ibovespaPercentageNode = doc.DocumentNode.SelectNodes("//div[@class='percentage']/p[@class='positive']");
+                //Passa como path o que queremos pegar no site
+                //No caso ele vai selecionar a div com class=value e o p dentro dela
+                var ibovespaPointsNode = doc.DocumentNode.SelectNodes("//div[@class='value']/p");
+                var ibovespaPercentageNodePositive = doc.DocumentNode.SelectNodes("//div[@class='percentage']/p[@class='positive']");
+                var ibovespaPercentageNodeNegative = doc.DocumentNode.SelectNodes("//div[@class='percentage']/p[@class='negative']");
 
-            //Na primeira posição da var, pega o valor do texto, remove os espaços e transforma em int
-            var ibovespaTotalPoints = ibovespaPointsNode[0].InnerText.Trim();
-            var ibovespaTotalPointsParse = double.Parse(ibovespaTotalPoints.ToString(), CultureInfo.InvariantCulture);
+                //Na primeira posição da var, pega o valor do texto, remove os espaços e transforma em int
+                var ibovespaTotalPoints = ibovespaPointsNode[0].InnerText.Trim();
+                var ibovespaTotalPointsParse = double.Parse(ibovespaTotalPoints.ToString(), CultureInfo.InvariantCulture);
 
-            var ibovespaPercentageVariation = ibovespaPercentageNode[0].InnerText.Trim();
+                var ibovespaPercentageVariation = "";
 
-            return new IbovespaInfoDTO(ibovespaTotalPointsParse, ibovespaPercentageVariation);
+                if (ibovespaPercentageNodePositive != null)
+                {
+                    ibovespaPercentageVariation = ibovespaPercentageNodePositive[0].InnerText.Trim();
+                }
+                else
+                {
+                    ibovespaPercentageVariation = ibovespaPercentageNodeNegative[0].InnerText.Trim();
+                }
+
+                return new IbovespaInfoDTO(ibovespaTotalPointsParse, ibovespaPercentageVariation);
+            }
+            catch (Exception)
+            {
+                throw new Exception("Desculpe ocorreu algum erro!");
+            }
         }
     }
 }
